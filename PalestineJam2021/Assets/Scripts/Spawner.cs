bd69fe645d6f1e6f799ad011858f746189a67112
Omar Khashoggi <10570290+OmarWKH@users.noreply.github.com>
2021-06-22 01:33:37 +0300

    spawner

---
+++ PalestineJam2021/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private int number = 1;
+    // TODO spawnRadius is same as targetRadius in SwarmMovement.cs
+    [SerializeField] private float spawnRadius = 3f;
+    [SerializeField] private GameObject prefab;
+
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Vector2 position = Random.insideUnitCircle * spawnRadius;
+            GameObject spawned = Instantiate(prefab, position, Quaternion.identity, transform);
+            spawned.name = $"{prefab.name}_{i}";
+        }
+    }
+}
